715d58a8edc25b74df758cdc68c4168b15443dbd
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-01-15 10:16:54 +0530

    QS project section created

--- src/pages/QsHome.jsx
+++ src/pages/QsHome.jsx
@@ -6,14 +6,16 @@ import OurTestimonials from "../sections/our-testimonials";
 import TrustedCompanies from "../sections/trusted-companies";
 import SubscribeNewsletter from "../sections/subscribe-newsletter";
 import JobPostSection from "../sections/job-post-section";
+import ProjectsSection from "../sections/ProjectsSection";
 
 const QsHome = () => {
   return (
     <>
       <main className="px-6 md:px-16 lg:px-24 xl:px-32">
         <HeroSection />
-        <OurLatestCreation />
+        {/* <OurLatestCreation /> */}
         <AboutOurApps />
+        <ProjectsSection />
         <OurTestimonials />
         <TrustedCompanies />
         <JobPostSection />
