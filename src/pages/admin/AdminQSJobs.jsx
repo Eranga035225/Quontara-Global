7368b3e42b07635e7f908ab5f36014142fc38b2e
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-01-22 23:32:37 +0530

    Added QS job ID in admin dashboard

--- src/pages/admin/AdminQSJobs.jsx
+++ src/pages/admin/AdminQSJobs.jsx
@@ -192,7 +192,7 @@ export default function AdminQSJobsPage() {
                         className="border-b border-gray-200 hover:bg-gray-50 transition"
                       >
                         <td className="py-3 px-4 text-gray-700 font-mono">
-                          {item._id}
+                          {item.qsJobId}
                         </td>
 
                         <td className="py-3 px-4 text-gray-900 font-medium">
